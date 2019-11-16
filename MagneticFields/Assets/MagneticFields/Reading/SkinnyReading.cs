4a1a46c7fb862a6abf78b8f4432e1ebdf95d4781
Erik Brobyn <erik.brobyn@icloud.com>
2019-11-16 13:01:36 -0500

    compile

--- MagneticFields/Assets/MagneticFields/Reading/SkinnyReading.cs
+++ MagneticFields/Assets/MagneticFields/Reading/SkinnyReading.cs
@@ -11,6 +11,10 @@ namespace MagneticFields.Reading
 {
     public class SkinnyReading : MonoBehaviour
     {
+        private List<LineRenderer> axes;
+        public float unitLength = 0.5f;
+        
+
         private Material vectorMaterial;
         private Vector3 reading;
         private Vector3 position;
@@ -54,7 +58,7 @@ namespace MagneticFields.Reading
                 Quaternion q = Utils.RotateA2B(a, b);
                 this.gameObject.transform.rotation = q;
                 this.gameObject.transform.localScale =
-                    new Vector3(.3f, .075f * (float)Math.Log(reading.magnitude), .3f);             
+                    new Vector3(.3f, .075f * (float)Math.Log(reading.magnitude), .3f);
             }
             get { return reading; }
         }
@@ -81,5 +85,79 @@ namespace MagneticFields.Reading
                 vectorMaterial.SetColor("_Color", color);
             }
         }
+
+
+        public float widthMultiplier
+        {
+            set
+            {
+                foreach (var a in axes)
+                {
+                    a.widthMultiplier = value;
+                }
+            }
+            get
+            {
+                return axes[0].widthMultiplier;
+            }
+        }
+        /*
+        public Axis()
+        {
+            axes = new List<LineRenderer>();
+
+            var xAxisObject = new GameObject();
+            xAxisObject.transform.parent = this.gameObject.transform;
+            axes.Add(xAxisObject.AddComponent<LineRenderer>());
+            axes[0].material = new Material(Shader.Find("Sprites/Default"));
+            axes[0].material.SetColor("_Color", Color.red);
+
+            var yAxisObject = new GameObject();
+            yAxisObject.transform.parent = this.gameObject.transform;
+            axes.Add(yAxisObject.AddComponent<LineRenderer>());
+            axes[1].material = new Material(Shader.Find("Sprites/Default"));
+            axes[1].material.SetColor("_Color", Color.green);
+
+            var zAxisObject = new GameObject();
+            zAxisObject.transform.parent = this.gameObject.transform;
+            axes.Add(zAxisObject.AddComponent<LineRenderer>());
+            axes[2].material = new Material(Shader.Find("Sprites/Default"));
+            axes[2].material.SetColor("_Color", Color.blue);
+
+            foreach (var a in axes)
+            {
+                a.widthMultiplier = 0.02f;
+                a.positionCount = 2;
+            }
+        }
+        */
+        public void OnDisable()
+        {
+            foreach (var a in axes)
+            {
+                a.enabled = false;
+            }
+        }
+
+        public void OnEnable()
+        {
+            foreach (var a in axes)
+            {
+                a.enabled = true;
+            }
+
+        }
+
+        public void Translate(Vector3 t)
+        {
+            foreach (var a in axes)
+            {
+                a.SetPosition(0, t);
+            }
+            axes[0].SetPosition(1, new Vector3(unitLength, 0, 0) + t);
+            axes[1].SetPosition(1, new Vector3(0, unitLength, 0) + t);
+            axes[2].SetPosition(1, new Vector3(0, 0, unitLength) + t);
+        }
+        
     }
 }
