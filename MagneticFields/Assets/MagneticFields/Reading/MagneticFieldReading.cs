44b8b0a19195c5ecc0ed99922365ec4fbd6af1d9
Erik Brobyn <erik.brobyn@icloud.com>
2019-11-28 13:34:43 -0500

    static class MagneticFieldReading

--- MagneticFields/Assets/MagneticFields/Reading/MagneticFieldReading.cs
+++ MagneticFields/Assets/MagneticFields/Reading/MagneticFieldReading.cs
@@ -6,6 +6,7 @@ namespace MagneticFields.Reading
     public static class MagneticFieldReading
     {
         public static readonly Vector3 RawVector = Input.compass.rawVector;
-        public static readonly float magneticHeading = Input.compass.magneticHeading;
+        public static readonly float MagneticHeading = Input.compass.magneticHeading;
+        public static readonly Transform CameraTransform = Camera.current.transform;
     }
 }
